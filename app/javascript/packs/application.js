2004216572b0715cc354d0f3645f514598eefdbf
vagrant <vagrant@localhost.localdomain>
2021-03-28 23:34:32 +0900

    ログインフォーム完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
 import 'jquery'
+import 'popper.js'
+import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
 import '../src/application.scss'
 
