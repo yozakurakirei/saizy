8eb00140ec50488caef3a63d6afd7cd0911aadc4
vagrant <vagrant@localhost.localdomain>
2021-04-01 00:37:55 +0900

    headerデザイン

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,29 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import 'jquery'
-import 'popper.js'
-import 'bootstrap'
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+require("jquery")
+require("popper.js")
+require("bootstrap")
+
+
+
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+// import 'jquery'
+// import 'popper.js'
+// import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
 import '../src/application.scss'
 
 
 
-Rails.start()
-// Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// // Turbolinks.start()
+// ActiveStorage.start()
