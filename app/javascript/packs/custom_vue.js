0619d3239c45c2396bb3bc0013ed2c48a10e2a8b
vagrant <vagrant@localhost.localdomain>
2021-04-04 22:02:27 +0900

    vue練習

--- app/javascript/packs/custom_vue.js
+++ app/javascript/packs/custom_vue.js
@@ -1,3 +1,22 @@
+// ローカルストレージへ保存
+// https://jp.vuejs.org/v2/examples/todomvc.html
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
@@ -5,18 +24,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#app',
     data: {
-      number:0, 
-      items: [1,2,3],
-      color: 'blue',
+      // 使用するデータ
+      todos: []
     },
     methods: {
-      countUp:function(){
-        this.number = this.number + 1
-      }
-    },
-    computed: {
-      custom_color:function(){
-        return this.color.toUpperCase();
+      // 使用するメソッド
+      doAdd: function(event, value) {
+        const comment = this.$refs.comment
+        if (!comment.value.length) {
+          return
+        }
+        this.todos.push({
+          id: todoStorage.uid++,
+          comment: comment.value,
+          state: 0
+        })
+        // フォームをからにする
+        comment.value = ""
       }
     },
     components: { App }
