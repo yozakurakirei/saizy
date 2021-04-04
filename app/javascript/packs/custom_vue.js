aa8ebbbf916d8ebe81f21139eba70bf81382a43f
vagrant <vagrant@localhost.localdomain>
2021-04-04 16:22:56 +0900

    vue.js vue.htmlに記載

--- app/javascript/packs/custom_vue.js
+++ app/javascript/packs/custom_vue.js
@@ -3,36 +3,21 @@ import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    el: '#hello2',
+    el: '#app',
     data: {
-      message: "ちゃんとSaizyの声が聞こえてますか?これはHTMLじゃなくてVue.jsからなんです"
+      number:0, 
+      items: [1,2,3],
+      color: 'blue',
     },
-    components: { App }
+    methods: {
+      countUp:function(){
+        this.number = this.number + 1
+      }
+    },
+    computed: {
+      custom_color:function(){
+        return this.color.toUpperCase();
+      }
+    }
   })
 })
-//
-//
-//
-// If the project is using turbolinks, install 'vue-turbolinks':
-//
-// yarn add vue-turbolinks
-//
-// Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
