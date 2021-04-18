29d02ffde77900e88374288862514548b6da7e86
yozakura <matchnokaori@gmail.com>
2021-04-19 00:02:36 +0900

    add: second-slider実装

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,11 +1,25 @@
 // slick-slider
+
+// first-slider 
 $(function() {
-  $('.slider-one').slick({
+  $('.slider-one')
+  .not("slick-intialized")
+  .slick({
     dots: true,
     prevArrow: ".site-slider .slider-btn .prev",
     nextArrow: ".site-slider .slider-btn .next"
   });
 });
 
-
+// second-slider
+$(function() {
+  $('.slider-two')
+  .not("slick-intialized")
+  .slick({
+    prevArrow: ".site-slider-two .prev",
+    nextArrow: ".site-slider-two .next",
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
+});
 
