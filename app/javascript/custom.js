87cae1bd6a21c6787d1ede2804dedd44dc9f63bc
yozakura <matchnokaori@gmail.com>
2021-04-18 21:39:00 +0900

    add: slick-slider実装

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,7 +1,11 @@
-$(function(){
-  console.log("OK")
-})
+// slick-slider
+$(function() {
+  $('.slider-one').slick({
+    dots: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next"
+  });
+});
 
-// slider 
-$('.slider-one').slick();
+
 
