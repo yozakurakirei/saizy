e8dc641352e6beb7285fd368b5ccdef97abe5e11
yozakura <matchnokaori@gmail.com>
2021-05-01 21:38:43 +0900

    add: vue componentをerbに成功

--- app/javascript/packs/custom_vue.js
+++ app/javascript/packs/custom_vue.js
@@ -1,47 +1,11 @@
-// ローカルストレージへ保存
-// https://jp.vuejs.org/v2/examples/todomvc.html
-var STORAGE_KEY = 'todos-vuejs-demo'
-var todoStorage = {
-  fetch: function() {
-    var todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || '[]'
-    )
-    todos.forEach(function(todo, index) {
-      todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
-  },
-  save: function(todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-  }
-}
-
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    el: '#app',
+    el: '#app2',
     data: {
-      // 使用するデータ
-      todos: []
-    },
-    methods: {
-      // 使用するメソッド
-      doAdd: function(event, value) {
-        const comment = this.$refs.comment
-        if (!comment.value.length) {
-          return
-        }
-        this.todos.push({
-          id: todoStorage.uid++,
-          comment: comment.value,
-          state: 0
-        })
-        // フォームをからにする
-        comment.value = ""
-      }
+      message: "新しいmain.jsだす"
     },
     components: { App }
   })
--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -22,13 +22,11 @@ import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    el: '#app',
+    el: '#hello',
     data: {
       message: "Can you say hello?"
     },
-    components: { 
-      App
-     }
+    components: { App }
   })
 })
 
