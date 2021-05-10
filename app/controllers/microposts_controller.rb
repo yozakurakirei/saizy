92898eaff152a256c4f70edf306f379680f8fe85
yozakura <matchnokaori@gmail.com>
2021-05-11 01:40:59 +0900

    add: ActiveRecord 複数画像実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user, only: :destroy
-z
+
   def create
     @micropost = current_user.microposts.build(micropost_params)
     @micropost.image.attach(params[:micropost] [:image])
