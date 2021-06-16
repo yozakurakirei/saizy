3277d0b5915630c709d0227dd1b8cd7f222b22b2
yozakura <matchnokaori@gmail.com>
2021-06-16 23:57:29 +0900

    add: user show previewエリア実装OK

--- app/javascript/packs/saizy_vue.js
+++ app/javascript/packs/saizy_vue.js
@@ -10,4 +10,52 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     components: { App }
   })
+
+  const single = new Vue({
+    el: "#single-preview",
+    data: function() {
+      return {
+        preview: null,
+        image: null,
+        preview_list: [],
+        image_list: []
+      };
+    },
+    methods: {
+      previewImage: function(event) {
+        const input = event.target;
+        if (input.files) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            this.preview = e.target.result;
+          }
+          this.image=input.files[0];
+          reader.readAsDataURL(input.files[0]);
+        }
+      },
+      previewMultiImage: function(event) {
+        const input = event.target;
+        const count = input.files.length;
+        const index = 0;
+        if (input.files) {
+          while(count --) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              this.preview_list.push(e.target.result);
+            }
+            this.image_list.push(input.files[index]);
+            reader.readAsDataURL(input.files[index]);
+            index ++;
+          }
+        }
+      },
+      reset: function() {
+        this.image = null;
+        this.preview = null;
+        this.image_list = [];
+        this.preview_list = [];
+      }
+    }
+  });
+  
 })
