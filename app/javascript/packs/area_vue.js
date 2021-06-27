d0a2dc4560a1f9cbe350d063e22bf487c519b158
yozakura <matchnokaori@gmail.com>
2021-06-28 00:08:11 +0900

    index pageレイアウト修正

--- app/javascript/packs/area_vue.js
+++ app/javascript/packs/area_vue.js
@@ -1,25 +1,25 @@
-import Vue from 'vue/dist/vue.esm'
+// import Vue from 'vue/dist/vue.esm'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const area = new Vue({
-    el: "#area_list",
-    data: {
-      list: [
-        { id: 1, areaName: "東京都", imageSrc: '/sinjyuku.jpeg' },
-        { id: 1, areaName: "神奈川県", imageSrc: './media/images/yokohama.jpeg' }
-      ],
-      count: 3,
-    },
-    computed: {
-      listItems() {
-        const list = this.list
-        return list.slice(0, this.count)
-      }
-    },
-    methods: {
-      isMore() {
-        this.count += 3
-      }
-    }
-  }) 
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const area = new Vue({
+//     el: "#area_list",
+//     data: {
+//       list: [
+//         { id: 1, areaName: "東京都", imageSrc: '/sinjyuku.jpeg' },
+//         { id: 1, areaName: "神奈川県", imageSrc: './media/images/yokohama.jpeg' }
+//       ],
+//       count: 3,
+//     },
+//     computed: {
+//       listItems() {
+//         const list = this.list
+//         return list.slice(0, this.count)
+//       }
+//     },
+//     methods: {
+//       isMore() {
+//         this.count += 3
+//       }
+//     }
+//   }) 
+// });
--- app/javascript/packs/like_vue.js
+++ app/javascript/packs/like_vue.js
@@ -1,22 +1,22 @@
-// いいねボタン
+// // いいねボタン
 
-import Vue from 'vue/dist/vue.esm'
+// import Vue from 'vue/dist/vue.esm'
 
-const VueStar = window['VueStar'];
-document.addEventListener('DOMContentLoaded', () => {
-  Vue.component('VueStar', VueStar) 
-  // いいねボタン
-  const like = new Vue ({
-    el: '#like',
-    mounted: function(){
-      this.$refs.ThumbsUp.$data.active = true;
-      console.log(this.$refs.ThumbsUp.$data);
-    },
-    methods: {
-      handleClick () {
-        console.log(this.$refs.ThumbsUp.$data);
-        //do something
-      }
-    }
-  })
-})
+// const VueStar = window['VueStar'];
+// document.addEventListener('DOMContentLoaded', () => {
+//   Vue.component('VueStar', VueStar) 
+//   // いいねボタン
+//   const like = new Vue ({
+//     el: '#like',
+//     mounted: function(){
+//       this.$refs.ThumbsUp.$data.active = true;
+//       console.log(this.$refs.ThumbsUp.$data);
+//     },
+//     methods: {
+//       handleClick () {
+//         console.log(this.$refs.ThumbsUp.$data);
+//         //do something
+//       }
+//     }
+//   })
+// })
